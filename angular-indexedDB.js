ebc107455501de51cd5df9e9696cadd6555e5582
gdi2290 <github@gdi2290.com>
2014-02-03 00:02:06 -0800

    update source

--- angular-indexedDB.js
+++ angular-indexedDB.js
@@ -1,11 +1,16 @@
 !function(window, angular, undefined) {
   'use strict';
-
   angular.module('angular-indexedDB', ['ngIndexedDB']);
 
-  angular.module('ngIndexedDB', [])
-  .factory('IndexedDB', function($window, $q) {
 
+  angular.module('ngIndexedDB', [])
+  .provider('$indexedDB', function() {
+    var module = this;
+
+    module.$get = function($window, $q) {
+      var indexedDB   = $window.indexedDB  || $window.mozIndexedDB  || $window.webkitIndexedDB  || $window.msIndexedDB;
+      var IDBKeyRange = $window.IDBKeyRange|| $window.mozIDBKeyRange|| $window.webkitIDBKeyRange|| $window.msIDBKeyRange;
+    };
   });
 
 }(this, this.angular);
