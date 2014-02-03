bed640beee1940803c1244c391bd4f926a9d988e
gdi2290 <github@gdi2290.com>
2014-02-02 22:27:29 -0800

    update source

---
+++ angular-indexedDB.js
@@ -0,0 +1,11 @@
+!function(window, angular, undefined) {
+  'use strict';
+
+  angular.module('angular-indexedDB', ['ngIndexedDB']);
+
+  angular.module('ngIndexedDB', [])
+  .factory('IndexedDB', function() {
+
+  });
+
+}(this, this.angular);
